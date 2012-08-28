58d2e38a79ac1966629245f0e34f743d25d46aaf
Jesús Martínez Novo <martineznovo@gmail.com>
2012-08-28 21:22:35 +0200

    Arreglos para oasis (ya aplicados, no estaban subidos)

--- DisableFirstSubmit/DisableFirstSubmit.js
+++ DisableFirstSubmit/DisableFirstSubmit.js
@@ -1,6 +1,6 @@
 // <pre>
 /*
- * DisableFirstSubmit v2.0: Clase para deshabilitar el botón de guardar la primera vez que se edita un artículo.
+ * DisableFirstSubmit v2.4: Clase para deshabilitar el botón de guardar la primera vez que se edita un artículo.
  * No lo deshabilita físicamente, pero en lugar de guardar avisará al usuario con un mensaje que obtiene de una página del wiki.
  * Copyright (C) 2009 - 2012 Jesús Martínez Novo ([[User:Ciencia Al Poder]])
  * This program is free software; you can redistribute it and/or modify
@@ -10,36 +10,52 @@
  * @requires: jquery.ui.dialog, mediawiki.api
  */
 (function($) {
-	var _init = function() {
+	var _dlg = null,
+		_init = function() {
 			$('#wpSave').bind('click', _onSave);
+			// oasis hack
+			$('#wpPreview').add('#wpDiff').bind('click.disablefirstsubmit', _onPreview);
 		},
 		_onSave = function() {
+			var api, params;
+			if (_dlg == null) {
+				api = new mw.Api();
+				params = {
+					action: 'parse',
+					page: 'MediaWiki:Common.js/Clases/DisableFirstSubmit.js/Userpage',
+					prop: 'text',
+					lang: mw.config.get('wgUserLanguage', mw.config.get('wgContentLanguage')),
+					disablepp: ''
+				};
+				api.get(params, { ok: _displayDialog, err: _endError } );
+			} else {
+				_dlg.dialog('open');
+			}
 			$('#wpSave').attr('disabled', 'disabled');
-			params = {
-				action: 'parse',
-				page: 'MediaWiki:Common.js/Clases/DisableFirstSubmit.js/Userpage',
-				prop: 'text',
-				lang: mw.config.get('wgUserLanguage', mw.config.get('wgContentLanguage')),
-				disablepp: ''
-			};
-			api = new mw.Api();
-			api.get(params, { ok: _displayDialog, err: _end } );
 			return false;
+		},
+		// oasis hack
+		_onPreview = function() {
+			$('#wpSave').unbind('click', _onSave);
+			$('#wpPreview').add('#wpDiff').unbind('click.disablefirstsubmit');
 		},
 		_displayDialog = function(data) {
 			if (data.error) {
 				_end();
 				return;
 			}
-			$('<div></div>').html(data.parse.text['*']).dialog({
+			_dlg = $('<div></div>').html(data.parse.text['*']).dialog({
 				modal: true,
 				title: 'Atención',
 				width: $(document).width()*0.75,
 				close: _end
 			});
 		},
-		_end = function() {
+		_endError = function() {
 			$('#wpSave').unbind('click', _onSave);
+			_end();
+		},
+		_end = function() {
 			$('#wpSave').removeAttr('disabled');
 		};
 
