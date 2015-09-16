f921ad8083ceccdcb4105b6cb6d3156ca0ed7723
Jesús Martínez Novo <martineznovo@gmail.com>
2015-09-16 23:04:25 +0200

    Quitando "hack para oasis" que ya no hace falta
    Esto deberia solucionar el double-encoding de atributos data- de Wikia

--- ThickBox4MediaWiki/thickbox.js
+++ ThickBox4MediaWiki/thickbox.js
@@ -143,20 +143,12 @@ window.Thickbox = (function($, mw) {
 	},
 	_showImage = function(elem) {
 		try {
-			var url, descUrl, descTitle, TB_secondLine = '', TB_descLink;
+			var url, descUrl, TB_secondLine = '', TB_descLink;
 			_preload();
 			elem = $(elem);
 
 			url = _getUrlFromThumb( elem.find('> img').eq(0).attr('src') );
 			descUrl = elem.attr('href');
-			// hack para oasis:
-			descTitle = elem.find('> img').data('image-name');
-			if (!descTitle) {
-				descTitle = elem.data('image-name');
-			}
-			if (descTitle) {
-				descUrl = mw.util.wikiGetlink('File:' + descTitle);
-			}
 			TB_descLink = '<a id="TB_descLink" class="sprite details" title="Ir a la página de descripción de la imagen"></a>';
 			// Se trata de un gallery?
 			if (_galleryIndex != -1) {
